0e10f2e6076d3821f33735f421915e676c5f5b89
Joshua Mummert <jm@joshuamummert.com>
2020-04-25 16:19:20 -0400

    create structs for http related methods, set up routing logic for future endpoints, register GET products endpoint

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 const http = require('http');
 
+const { products: products_data, search_meta: products_meta } = require('./products.json')
+
 const { CONTAINER_API_PORT, LOCALHOST_API_PORT } = process.env
 
 const logr = msg => console.log(`>> SERVER: ${msg}`)
 
+class JSONResponse {
+	constructor(meta, data) {
+		this.meta = meta
+		this.data = data
+	}
+}
+
+class Endpoint {
+	constructor(method, path, handler) {
+		this.method = method
+		this.path = path
+		this.handler = handler
+	}
+}
+
+let endpoints = [
+	new Endpoint('GET', '/products', getProducts)
+]
+
+function notFound(req, res) {
+	res.writeHead(404, { 'Content-Type': 'application.json' });
+	var json = JSON.stringify(new JSONResponse({ message: `Endpoint is not registered`, method: req.method, url: req.url}));
+	res.end(json)
+}
+
+function getProducts(req, res) {
+	res.writeHead(200, { 'Content-Type': 'application.json' });
+	var json = JSON.stringify(new JSONResponse(products_meta, products_data));
+	res.end(json)
+}
+
 http
 	.createServer((req, res) => {
-		res.writeHead(200, { 'Content-Type': 'application.json' });
-		res.end(JSON.stringify({ foo: 1 }));
+		let endpoint = endpoints.filter(e => e.method == req.method && RegExp(`^${e.path}$`).test(req.url))
+		endpoint = endpoint[0]
+
+		if (endpoint === undefined) return notFound(req, res)
+		
+		try {
+			const { handler } = endpoint
+
+			handler(req, res);
+		}
+		catch(error) {
+			console.error(`Endpoint ${endpoint.method} ${endpoint.path}: ${error}`)
+		}
 	})
 	.listen(CONTAINER_API_PORT, () => {
 		logr(`Docker container exposes port - ${CONTAINER_API_PORT} `)
